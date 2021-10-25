2b1e83a115308bb399b6daafc118fea067c31bdf
Camfez92 <62020207+Camfez92@users.noreply.github.com>
2021-10-25 00:55:32 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+class Asiento:
+    def __init__(self,color,precio,registro):
+        self.color=color
+        self.precio=precio
+        self.registro=registro
+    
+    def cambiarColor(self,color):
+        if color in ['rojo','verde','amarillo','negro','blanco']:
+            self.color=color
+
+class Motor:
+    def __init__(self,numeroCilindros,tipo,registro):
+        self.numeroCilindros=numeroCilindros
+        self.tipo=tipo
+        self.registro=registro
+
+    def cambiarRegistro(self,registro):
+        self.registro=registro
+    
+    def asignarTipo(self,tipo):
+        if tipo in ['gasolina','electrico']:
+            self.tipo=tipo
+
+class Auto:
+    cantidadCreados=0
+    
+    def __init__(self,modelo,precio,asientos,marca,motor,registro):
+        self.modelo=modelo
+        self.precio=precio
+        self.asientos=asientos
+        self.motor=motor
+        self.registro=registro
+
+    def cantidadAsientos(self):
+        cantAsientos=0
+        for asiento in self.asientos:
+            if type(asiento) is Asiento:
+                cantAsientos+=1
+    
+        return cantAsientos
+
+    def verificarIdentidad(self):
+        different=False
+        if self.motor.registro==self.registro:
+            for asiento in self.asientos:
+                if type(asiento) is Asiento:
+                    if asiento.registro!=self.registro:
+                        different=True
+                        break
+        else:
+            different=True
+        
+        if different:
+            return 'Las piezas no son originales”'
+        else:
+            return 'Auto original'
